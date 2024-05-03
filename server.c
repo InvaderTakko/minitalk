34f60a050be2d076590e8f51b2991ae0580b8d87
Stefan Ruff <ruff1stefan@gmail.com>
2024-05-03 13:56:04 +0200

    fixed get_str_len and some flags

--- server.c
+++ server.c
@@ -6,33 +6,34 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 16:16:10 by sruff             #+#    #+#             */
-/*   Updated: 2024/04/24 18:50:56 by sruff            ###   ########.fr       */
+/*   Updated: 2024/05/03 13:54:49 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static size_t get_str_len(int signum, size_t *str_len, size_t *bit_counter)
+static	size_t	get_str_len(int signum, size_t *str_len, size_t *bit_counter)
 {
-
-	if (*bit_counter < sizeof(size_t))
+	if (*bit_counter < (8 * sizeof(size_t)))
 	{
 		if (signum == SIGUSR1)
 		{
-			*str_len |= (size_t)1 << *bit_counter++;
-			if (*bit_counter == sizeof(size_t) * 8)
-			{
-
-				return (1);
-			}
+			*str_len |= (size_t)1 << *bit_counter;
+		}
+		(*bit_counter)++;
+		if (*bit_counter == sizeof(size_t) * 8)
+		{
+			return (1);
 		}
 	}
 	else
-		*bit_counter++;
+	{
+		(*bit_counter)++;
+	}
 	return (0);
 }
 
-static void  bit_to_char(int sig, char *c, int flag)
+static	void 	bit_to_char(int sig, char *c, int *flag)
 {
 	static int	bit;
 	
@@ -42,29 +43,28 @@ static void  bit_to_char(int sig, char *c, int flag)
 		*c = *c | (0 << bit);
 	bit++;
 	if (bit == 8)
-	{	
-		if (flag == 1)
-		{
-			bit = 0;
-			*c = 0;
-		}
+	{
+		*flag = 1;
+		bit = 0;
+		*c = 0;
 	}
 }
 
 // test later if i need void *context
 void sig_handler(int signum, siginfo_t *info, void *context)
 {
-	static size_t bit_counter;
-	static char *str;
-	static char *c;
+	static size_t bit_counter = 0;
+	static char *str = NULL;
+	static char c = 0;
 	static size_t str_len;
 	static int flag;
 	static int pid;
 
 	(void)context; // dont need it so we silence it
+	// ft_printf("do i get here\n");
 	if (pid == 0)
 		pid = info->si_pid;
-	if (get_str_len(signum, &str_len, &bit_counter) == 1)
+	if (get_str_len(signum, &str_len, &bit_counter) == 0)
 	{
 		if (str_len == 0)
 			exit(EXIT_SUCCESS); // maybe its a fail? ft_printf("Empty string\n")
@@ -78,22 +78,25 @@ void sig_handler(int signum, siginfo_t *info, void *context)
 	}
 	else if  (bit_counter > sizeof(size_t) * 8)
 	{	
-		bit_to_char(signum, c, flag);
-		if(bit_counter % 8 == 0)
+		bit_to_char(signum, &c, &flag);
+		if( flag == 1)
 		{
-			str[bit_counter - sizeof(size_t) * 8] = *c;
-			flag = 1;
-			
+			ft_printf("Char: %c\n", c);
+			str[bit_counter - sizeof(size_t) * 8] = c;
+			flag = 0;
+			c = 0;
+			ft_printf("String: %s\n", str);
+			if (bit_counter == (str_len) * sizeof(size_t) * 8)
+			{
+				ft_printf("String: %s\n", str);
+				free(str);
+				str = NULL;
+				bit_counter = 0;
+				str_len = 0;
+				pid = 0;
+			}
 		}
 	}
-	// once bitcounter == (str_len +1) * sizeof(size_t) * 8 we can print the string
-	// send signal to client that we are done
-	// and free the string
-
-	// if (signum == SIGUSR1)
-	// 	c |= 1 << bit_counter++;
-	// else if (signum == SIGUSR2)
-	// 	bit_counter++;
 }
 int	main(int argc, char **argv)
 {
@@ -103,6 +106,16 @@ int	main(int argc, char **argv)
 	sigemptyset(&sa.sa_mask); // initalize
 	sa.sa_flags = SA_RESTART | SA_SIGINFO; // set flags or inialize???
 	sa.sa_sigaction = sig_handler;
+    if (sigaction(SIGUSR1, &sa, NULL) == -1)
+	{
+		perror("sigaction");
+		return EXIT_FAILURE;
+	}
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+		perror("sigaction");
+		return EXIT_FAILURE;
+    }	
 	while (1)
 	{
 		// recieve_bits(); // useless since apparently pause invokes the signal handler
@@ -110,3 +123,13 @@ int	main(int argc, char **argv)
 	}
 	return (0);
 }
+
+
+	// once bitcounter == (str_len +1) * sizeof(size_t) * 8 we can print the string
+	// send signal to client that we are done
+	// and free the string
+
+	// if (signum == SIGUSR1)
+	// 	c |= 1 << bit_counter++;
+	// else if (signum == SIGUSR2)
+	// 	bit_counter++;
