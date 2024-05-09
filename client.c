9289a7cc13f8c79b799a859e9c061bf9d3dfaf0e
Stefan Ruff <ruff1stefan@gmail.com>
2024-05-09 14:08:49 +0200

    a bit of restructure and cleanup

--- client.c
+++ client.c
@@ -6,11 +6,25 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 16:16:23 by sruff             #+#    #+#             */
-/*   Updated: 2024/05/07 13:31:52 by sruff            ###   ########.fr       */
+/*   Updated: 2024/05/09 14:02:26 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	print_exit(char *message, int mode)
+{
+	if (mode == 1)
+	{
+		ft_printf("%s\n", message);
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		ft_printf("%s\n", message);
+		exit(EXIT_FAILURE);
+	}
+}
 
 void	send_str_len(pid_t pid, size_t len)
 {
@@ -23,12 +37,11 @@ void	send_str_len(pid_t pid, size_t len)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(100);
+		usleep(150);
 		bit++;
 	}
-	// ft_printf("Length sent\n");
 }
-// send the bits with magic
+
 void	send_bits(pid_t pid, char *str)
 {
 	int	i;
@@ -38,35 +51,24 @@ void	send_bits(pid_t pid, char *str)
 	while (str[i])
 	{
 		j = 0;
-		while (j < 8) // 8 bits in a byte
+		while (j < 8)
 		{
-			if ((str[i] & (1 << j)) != 0) // check if bit j is 1 and bitshift
+			if ((str[i] & (1 << j)) != 0)
 			{
-				// ft_printf("%d\n", j);
 				if (kill(pid, SIGUSR1) == -1)
-				{
-					ft_printf("Computer says no 1\n"); // pick better error message
-					exit(EXIT_FAILURE);
-				}
+					print_exit("Could not send signal", 0);
 			}
-			else // if bit is 0
+			else
 			{
 				if (kill(pid, SIGUSR2) == -1)
-				{
-					ft_printf("Computer says no 0\n"); // pick better error message
-					// perror("kill");
-					exit(EXIT_FAILURE);
-				}
+					print_exit("Could not send signal", 0);
 			}
-			// info->si_signo = ft_strlen(str);
 			j++;
-			usleep(100);// random sleep value 100 looks good
+			usleep(150);
 		}
 		i++;
 	}
 }
-
-
 
 // void	sig_handler_c(int n, siginfo_t *info, void *context)
 // {
@@ -82,65 +84,41 @@ void	send_bits(pid_t pid, char *str)
 // 	// 	ft_putstrnbr_fd("Num of bytes received -> ", i / 8);
 // }
 // check if server is alive
+
 void	recieve_answer(int signal, siginfo_t *info, void *context)
 {
 	(void)context;
 	(void)info;
 	if (signal == SIGUSR1)
-	{
-		ft_printf("Great Success\nVery nice\n"); // pick better message
-		exit(EXIT_SUCCESS);
-	}
+		print_exit("Server recieved message", 1);
 	else if (signal == SIGUSR2)
-	{
-		ft_printf("Blyat\n"); // pick better message
-		exit(EXIT_FAILURE);
-	}
+		print_exit("Error during sending", 0);
 }
+
 int	main(int argc, char **argv)
 {
 	struct sigaction	sa;
 	pid_t				pid;
-	siginfo_t			info;
-	
 
-	sigemptyset(&sa.sa_mask); // initalize
-	// sigaction = revieve_answer(getpid());
-	pid = ft_atoi(argv[1]);
-	// pid = 1234;
-	sa.sa_flags = SA_RESTART | SA_SIGINFO; // set flags or inialize???
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART | SA_SIGINFO;
 	if (argc == 3)
 	{
 		if (ft_strlen(argv[2]) == 0)
-		{
-			ft_printf("Error: Empty string\n");
-			return (1);
-		}
-
+			print_exit("Error: Empty string", 0);
 		pid = ft_atoi(argv[1]);
-		// sa.sa_handler = &recieve_answer; // set handler
 		sa.sa_sigaction = recieve_answer;
 		if (sigaction(SIGUSR1, &sa, NULL) == -1)
-			ft_printf("SIGURSR Error\n", 1);
+			print_exit("Could not send signal", 0);
 		if (sigaction(SIGUSR2, &sa, NULL) == -1)
-			ft_printf("SIGUSR Error\n", 1);
-		// ft_printf("PID : %d\n", pid);
+			print_exit("Could not send signal", 0);
+		ft_printf("PID : %d\n", pid);
 		send_str_len(pid, ft_strlen(argv[2]));
 		send_bits(pid, argv[2]);
-		(void)info;
 		while (1)
 			pause();
-		// pause(); // not sure if needed
-		EXIT_SUCCESS;
-
+		exit(EXIT_SUCCESS);
 	}
-	else
-	{
-			return (1);
-	}
-
+	print_exit("enter: ./client PID \"string\"", 0);
 	return (0);
 }
-
-// i am clueless if i can just send the string bit by bit and print it bit by bit
-// or if i need to check the length send the lenght decode the lenght and then malloc
--- server.c
+++ server.c
@@ -6,12 +6,32 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 16:16:10 by sruff             #+#    #+#             */
-/*   Updated: 2024/05/07 15:48:13 by sruff            ###   ########.fr       */
+/*   Updated: 2024/05/09 14:06:52 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+static	void	bit_to_char(int sig, t_server *s)
+{
+	static int	bit;
+
+	if (s->flag == 1)
+	{
+		s->flag = 0;
+		s->c = 0;
+	}
+	if (sig == SIGUSR1)
+		s->c = s->c | (1 << bit);
+	if (sig == SIGUSR2)
+		s->c = s->c | (0 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		s->flag = 1;
+		bit = 0;
+	}
+}
 // static void free_and_reset(t_server *s)
 // {
 // 	free(s->str);
@@ -22,6 +42,23 @@
 // 	s->c = 0;
 // 	s->flag = 0;
 // }
+// ft_printf("my calc %i\n", str_len * sizeof(char) * 8 + sizeof(size_t) * 8);
+
+static void	set_and_print_str(int sig, t_server *s)
+{
+	bit_to_char(sig, s);
+	if (s->flag == 1)
+	{
+		s->str[(s->bit_counter - sizeof(size_t) * 8) / 8 - 1] = s->c;
+		if (s->bit_counter == s->str_len * 8 + sizeof(size_t) * 8)
+		{
+			ft_printf("String: %s\n", s->str);
+			free(s->str);
+			kill(s->pid, SIGUSR1);
+			ft_memset(s, 0, sizeof(t_server));
+		}
+	}
+}
 
 static	size_t	get_str_len(int signum, t_server *s)
 {
@@ -43,37 +80,12 @@ static	size_t	get_str_len(int signum, t_server *s)
 	}
 	return (0);
 }
-// {
 
-
-static	void 	bit_to_char(int sig, t_server *s)
+void	sig_handler(int signum, siginfo_t *info, void *context)
 {
-	static int	bit;
+	static t_server	s = {0, NULL, 0, 0, 0, 0};
 
-	if (s->flag == 1)
-	{
-		s->flag = 0;
-		s->c = 0;
-	}
-	if (sig == SIGUSR1)
-		s->c = s->c | (1 << bit);
-	if (sig == SIGUSR2)
-		s->c = s->c | (0 << bit);
-	bit++;
-	if (bit == 8)
-	{
-		s->flag = 1;
-		bit = 0;
-	}
-}
-
-// test later if i need void *context
-void sig_handler(int signum, siginfo_t *info, void *context)
-{
-	static t_server s = {0, NULL, 0, 0, 0, 0};
-	
-	(void)context; // dont need it so we silence it
-	// ft_printf("do i get here\n");
+	(void)context;
 	if (s.pid == 0)
 		s.pid = info->si_pid;
 	if (s.pid != info->si_pid)
@@ -81,15 +93,11 @@ void sig_handler(int signum, siginfo_t *info, void *context)
 		kill(s.pid, SIGUSR2);
 		ft_printf("Wrong PID\n");
 		free(s.str);
-		// s = (t_server){0, NULL, 0, 0, 0, 0};
 		ft_memset(&s, 0, sizeof(t_server));
-		exit(EXIT_FAILURE); // either exit or resets statics+free and let it run
+		exit(EXIT_FAILURE);
 	}
 	if (get_str_len(signum, &s) == 1)
 	{
-		if (s.str_len == 0)
-			exit(EXIT_SUCCESS); // maybe its a fail? ft_printf("Empty string\n")
-		// ft_printf("String length: %i\n", str_len);	
 		s.str = (char *)malloc(sizeof(char) * (s.str_len + 1));
 		if (!s.str)
 		{
@@ -98,26 +106,10 @@ void sig_handler(int signum, siginfo_t *info, void *context)
 		}
 		s.str[s.str_len] = '\0';
 	}
-	if  (s.bit_counter > sizeof(size_t) * 8)
-	{
-		bit_to_char(signum, &s);
-		if( s.flag == 1)
-		{
-			// ft_printf("Char: %c\n", c);
-			s.str[(s.bit_counter - sizeof(size_t) * 8) / 8 - 1] = s.c;
-			// ft_printf("String: %s\n", str);
-			// ft_printf("Bit counter: %i\n", bit_counter);
-			// ft_printf("my calc %i\n", str_len * sizeof(char) * 8 + sizeof(size_t) * 8);
-			if (s.bit_counter == s.str_len * sizeof(char) * 8 + sizeof(size_t) * 8)
-			{
-				ft_printf("String: %s\n", s.str);
-				free(s.str);
-				kill(s.pid, SIGUSR1);
-				ft_memset(&s, 0, sizeof(t_server));
-			}
-		}
-	}
+	if (s.bit_counter > sizeof(size_t) * 8)
+		set_and_print_str(signum, &s);
 }
+
 int	main(int argc, char **argv)
 {
 	struct sigaction	sa;
@@ -125,33 +117,19 @@ int	main(int argc, char **argv)
 	(void)argv;
 	(void)argc;
 	ft_printf("PID: %d\n", getpid());
-	sigemptyset(&sa.sa_mask); // initalize
-	sa.sa_flags = SA_RESTART | SA_SIGINFO; // set flags or inialize???
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART | SA_SIGINFO;
 	sa.sa_sigaction = sig_handler;
-    if (sigaction(SIGUSR1, &sa, NULL) == -1)
-	{
-		perror("sigaction");
-		return EXIT_FAILURE;
-	}
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		return (EXIT_FAILURE);
 	if (sigaction(SIGUSR2, &sa, NULL) == -1)
-	{
-		perror("sigaction");
-		return EXIT_FAILURE;
-    }	
+		return (EXIT_FAILURE);
 	while (1)
 	{
-		// recieve_bits(); // useless since apparently pause invokes the signal handler
 		pause();
 	}
 	return (0);
 }
 
-
-	// once bitcounter == (str_len +1) * sizeof(size_t) * 8 we can print the string
-	// send signal to client that we are done
-	// and free the string
-
-	// if (signum == SIGUSR1)
-	// 	c |= 1 << bit_counter++;
-	// else if (signum == SIGUSR2)
-	// 	bit_counter++;
+// once bitcounter == (str_len +1) * sizeof(size_t) * 8 
+//we can print the string
