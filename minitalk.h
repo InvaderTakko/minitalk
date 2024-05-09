d540a57e73a39a08fa88f0589023b677f2f16596
Stefan Ruff <ruff1stefan@gmail.com>
2024-05-09 16:30:06 +0200

    added header

---
+++ minitalk.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/05 14:51:50 by sruff             #+#    #+#             */
+/*   Updated: 2024/05/09 16:27:09 by sruff            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "./libft/libft.h"
+# include "./libft/get_next_line.h"
+# include "./libft/ft_printf_bonus.h"
+# include <stdio.h>
+# include <signal.h>
+# include <unistd.h>
+
+typedef struct s_server {
+	size_t	bit_counter;
+	char	*str;
+	char	c;
+	size_t	str_len;
+	int		flag;
+	int		pid;
+}	t_server;
+
+
+#endif
