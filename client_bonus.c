f5854b7265c0ad20c3f3ec53f1d0ca1b951fce9b
Stefan Ruff <ruff1stefan@gmail.com>
2024-05-09 15:02:47 +0200

    same as none-bonus

--- client_bonus.c
+++ client_bonus.c
@@ -6,11 +6,25 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 15:53:27 by sruff             #+#    #+#             */
-/*   Updated: 2024/05/07 17:04:35 by sruff            ###   ########.fr       */
+/*   Updated: 2024/05/09 14:59:41 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	print_exit(char *message, int mode)
+{
+	if (mode == 1)
+	{
+		ft_printf("%s\n", message);
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		ft_printf("%s\n", message);
+		exit(EXIT_FAILURE);
+	}
+}
 
 void	send_str_len(pid_t pid, size_t len)
 {
@@ -23,12 +37,11 @@ void	send_str_len(pid_t pid, size_t len)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(100);
+		usleep(150);
 		bit++;
 	}
-	// ft_printf("Length sent\n");
 }
-// send the bits with magic
+
 void	send_bits(pid_t pid, char *str)
 {
 	int	i;
@@ -38,26 +51,20 @@ void	send_bits(pid_t pid, char *str)
 	while (str[i])
 	{
 		j = 0;
-		while (j < 8) // 8 bits in a byte
+		while (j < 8)
 		{
-			if ((str[i] & (1 << j)) != 0) // check if bit j is 1 and bitshift
+			if ((str[i] & (1 << j)) != 0)
 			{
 				if (kill(pid, SIGUSR1) == -1)
-				{
-					ft_printf("Computer says no 1\n"); // pick better error message
-					exit(EXIT_FAILURE);
-				}
+					print_exit("Could not send signal", 0);
 			}
 			else
-			{	
+			{
 				if (kill(pid, SIGUSR2) == -1)
-				{
-					ft_printf("Computer says no 0\n"); // pick better error message
-					exit(EXIT_FAILURE);
-				}
+					print_exit("Could not send signal", 0);
 			}
 			j++;
-			usleep(100);// random sleep value 100 looks good
+			usleep(150);
 		}
 		i++;
 	}
@@ -77,56 +84,41 @@ void	send_bits(pid_t pid, char *str)
 // 	// 	ft_putstrnbr_fd("Num of bytes received -> ", i / 8);
 // }
 // check if server is alive
+
 void	recieve_answer(int signal, siginfo_t *info, void *context)
 {
 	(void)context;
 	(void)info;
 	if (signal == SIGUSR1)
-	{
-		ft_printf("Great Success\nVery nice\n"); // pick better message
-		exit(EXIT_SUCCESS);
-	}
+		print_exit("Server recieved message", 1);
 	else if (signal == SIGUSR2)
-	{
-		ft_printf("Blyat\n"); // pick better message
-		exit(EXIT_FAILURE);
-	}
+		print_exit("Error during sending", 0);
 }
+
 int	main(int argc, char **argv)
 {
 	struct sigaction	sa;
 	pid_t				pid;
-	siginfo_t			info;
 
-	sigemptyset(&sa.sa_mask); // initalize
-	// sigaction = revieve_answer(getpid());
-	pid = ft_atoi(argv[1]);
+	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART | SA_SIGINFO;
 	if (argc == 3)
 	{
 		if (ft_strlen(argv[2]) == 0)
-		{
-			ft_printf("Error: Empty string\n");
-			return (1);
-		}
+			print_exit("Error: Empty string", 0);
 		pid = ft_atoi(argv[1]);
-		// sa.sa_handler = &recieve_answer; // set handler
 		sa.sa_sigaction = recieve_answer;
-		// if (sigaction(SIGUSR1, &sa, NULL) == -1)
-		// 	ft_printf("SIGURSR Error\n", 1);
-		// if (sigaction(SIGUSR2, &sa, NULL) == -1)
-		// 	ft_printf("SIGUSR Error\n", 1);
+		if (sigaction(SIGUSR1, &sa, NULL) == -1)
+			print_exit("Could not send signal", 0);
+		if (sigaction(SIGUSR2, &sa, NULL) == -1)
+			print_exit("Could not send signal", 0);
+		ft_printf("PID : %d\n", pid);
 		send_str_len(pid, ft_strlen(argv[2]));
 		send_bits(pid, argv[2]);
-		(void)info;
 		while (1)
 			pause();
-		EXIT_SUCCESS;
+		exit(EXIT_SUCCESS);
 	}
-	else
-		return (1);
+	print_exit("enter: ./client PID \"string\"", 0);
 	return (0);
 }
-
-// i am clueless if i can just send the string bit by bit and print it bit by bit
-// or if i need to check the length send the lenght decode the lenght and then malloc
