9d3dbccd560491c97a4976c5acd9e63dac37b8ec
Stefan Ruff <ruff1stefan@gmail.com>
2024-05-12 14:43:58 +0200

    remove comments

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 16:16:10 by sruff             #+#    #+#             */
-/*   Updated: 2024/05/09 16:22:36 by sruff            ###   ########.fr       */
+/*   Updated: 2024/05/12 14:39:56 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,7 @@ static	void	bit_to_char(int sig, t_server *s)
 		bit = 0;
 	}
 }
-// static void free_and_reset(t_server *s)
-// {
-// 	free(s->str);
-// 	s->str = NULL;
-// 	s->bit_counter = 0;
-// 	s->str_len = 0;
-// 	s->pid = 0;
-// 	s->c = 0;
-// 	s->flag = 0;
-// }
+
 // ft_printf("my calc %i\n", str_len * sizeof(char) * 8 + sizeof(size_t) * 8);
 
 static void	set_and_print_str(int sig, t_server *s)
--- server_bonus.c
+++ server_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 15:51:46 by sruff             #+#    #+#             */
-/*   Updated: 2024/05/09 16:23:27 by sruff            ###   ########.fr       */
+/*   Updated: 2024/05/12 14:41:24 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,7 @@ static	void	bit_to_char(int sig, t_server *s)
 		bit = 0;
 	}
 }
-// static void free_and_reset(t_server *s)
-// {
-// 	free(s->str);
-// 	s->str = NULL;
-// 	s->bit_counter = 0;
-// 	s->str_len = 0;
-// 	s->pid = 0;
-// 	s->c = 0;
-// 	s->flag = 0;
-// }
+
 // ft_printf("my calc %i\n", str_len * sizeof(char) * 8 + sizeof(size_t) * 8);
 
 static void	set_and_print_str(int sig, t_server *s)
